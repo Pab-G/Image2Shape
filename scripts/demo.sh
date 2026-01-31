9b5b1d18b0979697e97dabee1ec982044495c6b3
Pablo <pablo.vonbaum@icloud.com>
2026-01-31 12:26:17 +0100

    refract code

---
+++ scripts/demo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 1. Generate the 4 views with ImageDream
+# Using the spot example
+#export PYTHONPATH=$PYTHONPATH:../models/ImageDream/
+#conda run -n imagedream python ../models/ImageDream/scripts/demo.py --image "../demo/assets/spot/front_edit.png"     --text "a cow with a black hat, 3d asset"     --config_path "../models/ImageDream/imagedream/configs/sd_v2_base_ipmv.yaml"     --ckpt_path "../models/ImageDream/release_models/ImageDream/sd-v2.1-base-4view-ipmv.pt"     --mode "pixel"     --num_frames 5 --method Inversion
+
+# 2. Reconstruct 3D with SnapGTR
+conda run -n activate gtr python3 ../models/snap_gtr/scripts/prepare_mv.py --in_dir ../demo/outputs/ImageDreamDiffusion/Inversion_front_edit.png --out_dir ../models/snap_gtr/examples/spot/
+conda run -n activate gtr python3 ../models/snap_gtr/scripts/inference.py --ckpt_path ../models/snap_gtr/ckpts/full_checkpoint.pth --in_dir ../models/snap_gtr/examples/spot/--out_dir ../demo/outputs/3DAssets/spot/
+
